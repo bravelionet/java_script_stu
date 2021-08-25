32a33fb36adebd2dfc3e0e278d816301530db07a
lionet <bravelionet@163.com>
2021-08-25 14:26:00 +0800

    添加: 事件, 字符串, 数组

---
+++ src/事件/js/EventJs.js
@@ -0,0 +1,11 @@
+function appOnUpdate(){
+    document.getElementById("appOn").innerHTML="变字体了";
+}
+
+function appOnmouseout(){
+    document.getElementById("appOn").innerHTML="移动鼠标更改字体";
+}
+
+function appOnload(){
+    document.getElementById("appOn").innerHTML="onload sss";
+}
